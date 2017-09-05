95c8ea2b8fc73c4fdf7086ad81548a0108f4023e
Roman Vais <rvais@redhat.com>
2017-09-05 14:39:10 +0200

    New files to enable writing tests with pytest
    commons.py contains fixture to startup and exit browser so it is done
        only once for whole test group (tests in one class)
    commons.py enables to use fixture mentioned above so pytest will find it
        by itself.

---
+++ webtest/conftest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#
+# Framework for testing web applications - proof of concept
+# Authors:  Roman Vais <rvais@redhat.com>
+#
+
+import pytest
+from webtest.tests.commons import page_to_test
---
+++ webtest/tests/commons.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+# Framework for testing web applications - proof of concept
+# Authors:  Roman Vais <rvais@redhat.com>
+#
+from time import sleep
+
+import pytest
+from webtest.webagent import WebAgent
+from webtest.common.logger import get_logger
+
+@pytest.fixture(scope="module")
+def page_to_test(request):
+    logger = get_logger("Fixture")
+
+    logger.info("Setup phase.")
+
+    logger.debug("Instantiating WebAgent class.")
+    agent = WebAgent()
+
+    logger.debug("Getting browser name form the test case.")
+    browser_name = getattr(request.module, "browser_name", WebAgent.BROWSER_CHROME)
+
+    logger.debug("Starting up selected browser '{}'.".format(browser_name))
+    agent.start_up_browser(browser=browser_name)
+
+    logger.debug("Getting page model from the test case.")
+    model = getattr(request.module, "page_model", None)
+    if model is None:
+        logger.debug("No model found. Falling back to URL settings")
+        l = list()
+        for attribute in ["host_name", "port", "url", "protocol"]:
+            value = getattr(request.module, attribute, None)
+            if value is None and attribute != "host_name":
+                continue
+            elif value is None and attribute == "host_name":
+                raise Exception("Neither page model or url has not been provided")
+
+            l.append(value)
+
+        url = tuple(l)
+        page = agent.go_to_page(*url)
+
+    else:
+        page = agent.get_page(model=model)
+
+    sleep(2)
+
+#    return page
+    yield page
+
+    sleep(5)
+
+    logger.info("Tear-down phase.")
+    agent.close_browser()
