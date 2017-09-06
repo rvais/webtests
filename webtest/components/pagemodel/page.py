2165e3d1bd7c1dea2eff08e240e4d93962441e1a
Roman Vais <rvais@redhat.com>
2017-09-06 17:54:49 +0200

    Fix Page class and querying the root element

--- webtest/components/pagemodel/page.py
+++ webtest/components/pagemodel/page.py
@@ -6,6 +6,7 @@
 
 from webtest.components.pagemodel.model import PageModel
 from webtest.components.pagemodel.element import Element
+from webtest.components.pagemodel.mock_element import MockElement
 from webtest.common.logger import get_logger
 
 class Page(object):
@@ -16,15 +17,12 @@ class Page(object):
     def __init__(self, browser: 'Browser', model: PageModel=None):
         class_name = str(self.__class__.__name__)
         self._logger = get_logger(class_name)
+        self._root = None
 
         if browser is not None :
             self._browser = browser
             self._logger.debug("Browser '{}' is available.".format(browser.name))
-            raw_root = browser.driver.find_element_by_xpath(Page.ROOT_XPATH) # :Element
 
-            self._logger.debug("Root element found is <{}> and will be set as root element.".format(raw_root.tag_name))
-            self._root = Element(raw_root)
-            
         else:
             self._browser = None
             self._logger.warning("No browser available. May cause problems in following code.")
@@ -39,6 +37,16 @@ class Page(object):
         pass
 
     def get_root(self):
+
+        if self._browser is None and self._root is None:
+            self._root = MockElement()
+
+        elif self._root is None:
+            raw_root = self._browser.driver.find_element_by_xpath(Page.ROOT_XPATH)  # :Element
+
+            self._logger.debug("Root element found is <{}> and will be set as root element.".format(raw_root.tag_name))
+            self._root = Element(raw_root)
+
         self.__before_returning_element(self._root)
         return self._root
 
@@ -48,8 +56,12 @@ class Page(object):
         if component_name == Page.ROOT_COMPONENT_NAME:
             return self.get_root()
 
+        if component_name not in self._model.keys():
+            return None
+
         c = self._model[component_name]
-        c.set_root(self.get_root())
+        if isinstance(c.get_element_node(), MockElement) and not isinstance(self.get_root(), MockElement):
+            c.set_root(self.get_root())
         return c
 
     @property
