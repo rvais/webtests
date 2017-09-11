280285f8ff4e1362af2622926cfaae021b04dc88
Roman Vais <rvais@redhat.com>
2017-09-11 11:02:02 +0200

    Fix 'fill component' methods, improve logging

--- webtest/components/pagemodel/component.py
+++ webtest/components/pagemodel/component.py
@@ -4,6 +4,8 @@
 # Authors:  Roman Vais <rvais@redhat.com>
 #
 from copy import copy
+from selenium.common.exceptions import StaleElementReferenceException
+
 from webtest.components.pagemodel.element import WebElement
 from webtest.components.pagemodel.mock_element import MockElement
 from webtest.common.logger import get_logger
@@ -21,6 +23,8 @@ class Component(object):
         self._node = MockElement("node")
 
         self._subcomponents = list()
+
+        self._logger.debug("Component '{}' created.".format(self._name))
 
     @property
     def name(self):
@@ -51,18 +55,16 @@ class Component(object):
 
 
     def set_root(self, root: WebElement):
-        original_root = self._root
+        original_root = self._root # type: MockElement
         original_node = self._node
 
         try:
             self._logger.debug("Attempting to set new root node <{}> for this component.".format(root.tag_name))
             self._root = root
-            self._node = self._root.find_element(self._root, self._stype, self._svalue)
-
-            # if self._subcomponents
+            self._node = self._root.get_element(self._root, self._stype, self._svalue)
 
             for comp in self._subcomponents:
-                self._logger.debug("Setting root node for subcomponent '{}'.".format(str(comp)))
+                self._logger.debug("Setting root node for subcomponent '{}'.".format(str(comp.name)))
                 if not comp.set_root(self._node):
                     raise Exception()
 
@@ -80,6 +82,10 @@ class Component(object):
         return self._node
 
     def get_subcomponents(self):
+        for component in self._subcomponents:
+            if isinstance(component.get_element_node(), MockElement) and not isinstance(self._root, MockElement):
+                component.set_root(self._node)
+
         return copy(self._subcomponents)
 
     def get_subcomponent(self, name: str="", recursive: bool=False, depth: int=1):
@@ -104,28 +110,84 @@ class Component(object):
     def add_subcomponents(self, components: list):
         self._subcomponents.extend(components)
 
+
     def fill(self, value: dict):
-        self._logger.debug("Attempt to fill component {} with data.".format(self.name))
+        try:
+            return self.__fill(value)
+
+        except StaleElementReferenceException as ex:
+            self._logger.debug("Inner node is no longer in document. Trying to re-set inner node.")
+            self._logger.trace(ex)
+            self.set_root(self._root)
+            return self.__fill(value)
+
+    def __fill(self, value: dict):
+        self._logger.debug("Attempt to fill component {} with data {}.".format(self.name, value))
 
         if isinstance(value, dict):
             success = True
             for subcomponent, item in value.items():
                 self._logger.debug("component {}, data {}.".format(subcomponent, item))
-                success = success and self.get_subcomponent(subcomponent).fill(item)
+                c = self.get_subcomponent(subcomponent)
+                tmp = c.fill(item)
+                success = success and tmp
             return success
 
         else:
             return self._node.fill(value)
 
-    def fill_input(self, node_type: str, value, xpath: str=""):
-        return self._node.fill_input(self._node, node_type,value, xpath)
-
-    def fill_select(self, *options):
-        return self._node.fill_select(self._node,options)
 
     def fill_form(self, items: list):
+        try:
+            return self.__fill_form(items)
+
+        except StaleElementReferenceException as ex:
+            self._logger.debug("Inner node is no longer in document. Trying to re-set inner node.")
+            self._logger.trace(ex)
+            self.set_root(self._root)
+            return self.__fill_form(items)
+
+    def __fill_form(self, items: list):
         return self._node.fill_form(self._node, items)
 
-    def click(self):
-        return self._node.click(self._node)
 
+    def fill_input(self, node_type: str, value, xpath: str=""):
+        try:
+            return self.__fill_input(node_type, value, xpath)
+
+        except StaleElementReferenceException as ex:
+            self._logger.debug("Inner node is no longer in document. Trying to re-set inner node.")
+            self._logger.trace(ex)
+            self.set_root(self._root)
+            return self.__fill_input(node_type, value, xpath)
+
+    def __fill_input(self, node_type: str, value, xpath: str=""):
+        return self._node.fill_input(self._node, node_type,value, xpath)
+
+
+    def fill_select(self, *options):
+        try:
+            return self.__fill_select(self, *options)
+
+        except StaleElementReferenceException as ex:
+            self._logger.debug("Inner node is no longer in document. Trying to re-set inner node.")
+            self._logger.trace(ex)
+            self.set_root(self._root)
+            return self.__fill_select(self, *options)
+
+    def __fill_select(self, *options):
+        return self._node.fill_select(self._node,options)
+
+
+    def click(self):
+        try:
+            return self.__click()
+
+        except StaleElementReferenceException as ex:
+            self._logger.debug("Inner node is no longer in document. Trying to re-set inner node.")
+            self._logger.trace(ex)
+            self.set_root(self._root)
+            return self.__click()
+
+    def __click(self):
+        return self._node.click(self._node)
