d8e6fdd098da57597bce3d70bd87ec17bdc01865
Roman Vais <rvais@redhat.com>
2017-09-12 10:25:05 +0200

    Add RootComponent class, Update method order
    Add RootComponent class that serves as a container for page root
    node. It has only different constructor so it is subclass of
    Component and it is in the same file (against my better judgement).
    
    Order of methods in Component class has been changed.

--- webtest/components/pagemodel/component.py
+++ webtest/components/pagemodel/component.py
@@ -6,8 +6,10 @@
 from copy import copy
 from selenium.common.exceptions import StaleElementReferenceException
 
+from webtest.common.selector import Selector
 from webtest.components.pagemodel.element import WebElement
 from webtest.components.pagemodel.mock_element import MockElement
+from webtest.components.pagemodel.mock_element import Element
 from webtest.common.logger import get_logger
 
 class Component(object):
@@ -27,34 +29,34 @@ class Component(object):
         self._logger.debug("Component '{}' created.".format(self._name))
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def is_available(self):
+    def is_available(self) -> bool:
         return not isinstance(self._root, MockElement)
 
     @property
-    def selector_type(self):
+    def selector_type(self) -> str:
         return self._stype
 
     @property
-    def selector_value(self):
+    def selector_value(self) -> str:
         return self._svalue
 
     @property
-    def parent(self):
+    def parent(self) -> Element or MockElement:
         return self._parent
 
     @property
-    def tuple(self):
+    def tuple(self) -> tuple:
         if self._parent is None:
             return (self._name, self._stype, self._svalue)
         else:
             return (self._name, self._stype, self._svalue, self._parent)
 
 
-    def set_root(self, root: WebElement):
+    def set_root(self, root: Element or WebElement) -> bool:
         original_root = self._root # type: MockElement
         original_node = self._node
 
@@ -78,17 +80,17 @@ class Component(object):
 
         return True
     
-    def get_element_node(self):
+    def get_element_node(self) -> Element or MockElement:
         return self._node
 
-    def get_subcomponents(self):
+    def get_subcomponents(self) -> list:
         for component in self._subcomponents:
             if isinstance(component.get_element_node(), MockElement) and not isinstance(self._root, MockElement):
                 component.set_root(self._node)
 
         return copy(self._subcomponents)
 
-    def get_subcomponent(self, name: str="", recursive: bool=False, depth: int=1):
+    def get_subcomponent(self, name: str="", recursive: bool=False, depth: int=1) -> 'Component':
         if name is None or not len(name) > 0:
             return None
 
@@ -104,14 +106,14 @@ class Component(object):
 
         return found
 
-    def add_subcomponent(self, component: 'Component'):
+    def add_subcomponent(self, component: 'Component') -> None:
         self._subcomponents.append(component)
 
-    def add_subcomponents(self, components: list):
+    def add_subcomponents(self, components: list) -> None:
         self._subcomponents.extend(components)
 
 
-    def fill(self, value: dict):
+    def fill(self, value: dict) -> bool:
         try:
             return self.__fill(value)
 
@@ -121,7 +123,7 @@ class Component(object):
             self.set_root(self._root)
             return self.__fill(value)
 
-    def __fill(self, value: dict):
+    def __fill(self, value: dict) -> bool:
         self._logger.debug("Attempt to fill component {} with data {}.".format(self.name, value))
 
         if isinstance(value, dict):
@@ -137,7 +139,7 @@ class Component(object):
             return self._node.fill(value)
 
 
-    def fill_form(self, items: list):
+    def fill_form(self, items: list) -> bool:
         try:
             return self.__fill_form(items)
 
@@ -147,11 +149,11 @@ class Component(object):
             self.set_root(self._root)
             return self.__fill_form(items)
 
-    def __fill_form(self, items: list):
+    def __fill_form(self, items: list) -> bool:
         return self._node.fill_form(self._node, items)
 
 
-    def fill_input(self, node_type: str, value, xpath: str=""):
+    def fill_input(self, node_type: str, value, xpath: str="") -> bool:
         try:
             return self.__fill_input(node_type, value, xpath)
 
@@ -161,11 +163,11 @@ class Component(object):
             self.set_root(self._root)
             return self.__fill_input(node_type, value, xpath)
 
-    def __fill_input(self, node_type: str, value, xpath: str=""):
+    def __fill_input(self, node_type: str, value, xpath: str="") -> bool:
         return self._node.fill_input(self._node, node_type,value, xpath)
 
 
-    def fill_select(self, *options):
+    def fill_select(self, *options) -> bool:
         try:
             return self.__fill_select(self, *options)
 
@@ -175,11 +177,11 @@ class Component(object):
             self.set_root(self._root)
             return self.__fill_select(self, *options)
 
-    def __fill_select(self, *options):
+    def __fill_select(self, *options) -> bool:
         return self._node.fill_select(self._node,options)
 
 
-    def click(self):
+    def click(self) -> bool:
         try:
             return self.__click()
 
@@ -189,5 +191,18 @@ class Component(object):
             self.set_root(self._root)
             return self.__click()
 
-    def __click(self):
+    def __click(self) -> bool:
         return self._node.click(self._node)
+
+
+class RootComponent(Component):
+    def __init__(self, root: Element):
+        super(RootComponent, self).__init__(self, "root", Selector.XPATH, '//')
+
+        class_name = str(self.__class__.__name__)
+        self._logger = get_logger(class_name)
+
+        self._root = root
+        self._node = root
+
+        self._logger.debug("Root component '{}' created.".format(self._name))
