7d673659e6c47bc8425f08ecdf1386d10a6b1a00
Roman Vais <rvais@redhat.com>
2017-09-12 12:24:08 +0200

    Add method to derive template from existing one
    Add method that alows to duplicate and update template
    Remove responsibility to format url from this class and employ
        format_url method recently added

--- webtest/components/pagemodel/model.py
+++ webtest/components/pagemodel/model.py
@@ -5,16 +5,19 @@
 #
 
 from webtest.common.http import Constants as HTTP_CONST
+from webtest.common.http import format_url
 from webtest.components.pagemodel.component import Component
 from webtest.common.logger import get_logger
 
 class PageModel(dict):
 
-    def __init__(self, protocol: str=HTTP_CONST.PROTOCOL_HTTP, host: str="localhost", port: int=0, url: str="/", *arg, **kw):
-        super(PageModel, self).__init__(*arg, **kw)
+    def __init__(self, protocol: str=HTTP_CONST.PROTOCOL_HTTP, host: str="localhost", port: int=0, url: str="/", *arg, **kwargs):
+        super(PageModel, self).__init__(*arg, **kwargs)
 
         class_name = str(self.__class__.__name__)
         self._logger = get_logger(class_name)
+
+        self._name = class_name
 
         self._protocol = protocol
         self._host = host
@@ -57,26 +60,38 @@ class PageModel(dict):
 
         return
 
+    def __set_name(self, name: str):
+        self._name = name
+
     def _create_template(self):
         return list()
 
     @property
     def name(self):
-        return self.__class__.name
+        return self._name
 
     @property
     def url(self):
-        if len(self._url) > 0 and self._url[0] == '/':
-            self._url = self._url[1:]
+        return format_url(self._protocol, self._host, self._port, self._url)
 
-        if self._port <= 0:
-            url_format = '{}://{}/{}'
-            url = url_format.format(self._protocol, self._host, self._url)
-        else:
-            url_format = '{}://{}:{}/{}'
-            url = url_format.format(self._protocol, self._host, self._port, self._url)
+    def derive_template(
+            self,
+            name: str,
+            template: list,
+            protocol: str or None=None,
+            host: str or None=None,
+            port: int or None=None,
+            url: str or None=None,
+    ) -> 'PageModel':
+        protocol = protocol if protocol is not None else self._protocol
+        host = host if host is not None else self._host
+        port = port if port is not None else self._port
+        url = url if url is not None else self._url
+        new_model = PageModel(protocol, host, port, url)
 
-        return url
+        new_model.__set_name(name)
 
+        new_model.__fill_model(self._create_template())
+        new_model.__fill_model(template)
 
-
+        return new_model
