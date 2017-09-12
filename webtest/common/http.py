14921beb650d59f56acfa54c612dd34cb673e266
Roman Vais <rvais@redhat.com>
2017-09-12 12:13:28 +0200

    Add methods to format urls to http.py
    format_url method's resposibility has been removed form PageModel
    class so it is no longer necessary to create empty template just
    to have url formated from individual components.
    
    relax_url serves to remove HTTP_GET method variables (stuff that
    is following after question mark) and anchor text (text following
    the hash '#' sign) from url so templete paired with url can be
    used for changing page which does not change layout.

--- webtest/common/http.py
+++ webtest/common/http.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Framewor for testing web aplications - proof of concept
+# Framework for testing web applications - proof of concept
 # Authors:  Roman Vais <rvais@redhat.com>
 #
 
@@ -8,3 +8,27 @@ class Constants(object):
     PROTOCOL_HTTP = "http"
     PROTOCOL_HTTPS = "https"
 
+def format_url(protocol: str=Constants.PROTOCOL_HTTP, host: str='localhost', port: int=0, url:str='/'):
+    if len(url) > 0 and url[0] == '/':
+        url = url[1:]
+
+    if port <= 0:
+        url_format = '{}://{}/{}'
+        full_url = url_format.format(protocol, host, url)
+    else:
+        url_format = '{}://{}:{}/{}'
+        full_url = url_format.format(protocol, host, port, url)
+
+    return full_url
+
+def relax_url(full_url: str, relax_anchor: bool=True, relax_get_method: bool=True):
+    if relax_anchor:
+        index = full_url.rindex('#')
+        if index >= 0:
+            full_url = full_url[0 : index]
+    if relax_get_method:
+        index = full_url.rindex('?')
+        if index >= 0:
+            full_url = full_url[0 : index]
+
+    return full_url
