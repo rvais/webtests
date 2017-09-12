ad89136d5b6559561f641e3c53cfa38775098772
Roman Vais <rvais@redhat.com>
2017-09-12 10:13:57 +0200

    Add option to search for subcomponenets

--- webtest/components/pagemodel/page.py
+++ webtest/components/pagemodel/page.py
@@ -5,8 +5,10 @@
 #
 from time import sleep
 
+from webtest.browsers.browser import Browser
 from webtest.components.pagemodel.model import PageModel
 from webtest.components.pagemodel.element import Element
+from webtest.components.pagemodel.component import Component, RootComponent
 from webtest.components.pagemodel.mock_element import MockElement
 from webtest.common.logger import get_logger
 
@@ -19,6 +21,7 @@ class Page(object):
         class_name = str(self.__class__.__name__)
         self._logger = get_logger(class_name)
         self._root = None
+        self._root_component = None
 
         self._logger.info("Creating new instance of Page.")
 
@@ -29,17 +32,6 @@ class Page(object):
         else:
             self._browser = None
             self._logger.warning("No browser available. May cause problems in following code.")
-
-#        for x in range(3):
-#            try:
-#                if model is None or model.url != browser.current_url:
-#                    self._logger.warning("Page may have been redirected.")
-#                    raise Exception("Redirections!")
-#                else:
-#                    break
-#
-#            except Exception as ex:
-#                sleep(2)
 
         if model is None: # or model.url != browser.current_url:
             self._logger.warning("No page model available. May cause problems in following code.")
@@ -53,7 +45,7 @@ class Page(object):
     def __before_returning_element(self, getter):
         pass
 
-    def get_root(self):
+    def get_root(self) -> Element or MockElement:
 
         if self._browser is None and self._root is None:
             self._root = MockElement()
@@ -61,25 +53,58 @@ class Page(object):
         elif self._root is None:
             raw_root = self._browser.driver.find_element_by_xpath(Page.ROOT_XPATH)  # :Element
 
-            self._logger.debug("Root element found is <{}> and will be set as root element.".format(raw_root.tag_name))
+            self._logger.debug("Root element found is <{}> and will be set as "
+                               "root element.".format(raw_root.tag_name))
             self._root = Element(raw_root)
 
         self.__before_returning_element(self._root)
         return self._root
 
-    def get_component(self, component_name: str=ROOT_COMPONENT_NAME, subcomponent_namestr: str=None):
+    def get_component(
+            self,
+            component_name: str=ROOT_COMPONENT_NAME,
+            subcomponent_name: str or None=None,
+            search_depth: int=0
+    ) -> Component or None:
         self._logger.debug("Attempt ot get component '{}'.".format(component_name))
 
         if component_name == Page.ROOT_COMPONENT_NAME:
-            return self.get_root()
+            self._logger.debug("Returning root node component.")
+            if self._root_component is None:
+                self._root_component = RootComponent(self.get_root())
+            return self._root_component
 
         if component_name not in self._model.keys():
-            return None
+            self._logger.debug("Component not found between main page components.")
+            c = None # type: Component
+            if search_depth:
+                self._logger.debug("Searching given name in subcomponents "
+                                   "with recursive depth={}.".format(search_depth))
 
-        c = self._model[component_name]
-        if isinstance(c.get_element_node(), MockElement) and not isinstance(self.get_root(), MockElement):
+                for name, component in self._model.items():
+                    self._logger.debug("Searching in main component '{}'.".format(name))
+                    c = component.get_subcomponent(component_name, search_depth > 1, search_depth) # type: Component
+                    if c is not None:
+                        self._logger.debug("Component '{}' found under '{}'.".format(c.name, name))
+                        break
+
+        else:
+            c = self._model[component_name] # type: Component
+
+        if (isinstance(c.get_element_node(), MockElement)
+                and not isinstance(self.get_root(), MockElement)):
             self._logger.trace("Setting root <{}> for component '{}'.".format(self.get_root(), component_name))
             c.set_root(self.get_root())
+
+        if c is not None and subcomponent_name is not None and len(subcomponent_name) > 0:
+            self._logger.debug("Searching for subcomponent '{}' "
+                               "under component '{}'.".format(subcomponent_name, c.name))
+            c = c.get_subcomponent(component_name, search_depth > 1, search_depth)  # type: Component
+            if c is not None:
+                self._logger.debug("Subcomponent '{}' found.".format(c.name))
+            else:
+                self._logger.debug("Subcomponent '{}' was not found.".format(c.name))
+
         return c
 
     @property
