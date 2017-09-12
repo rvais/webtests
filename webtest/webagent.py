c1dfd10caff8bfb558c6b22b0230176c8995c290
Roman Vais <rvais@redhat.com>
2017-09-12 12:37:48 +0200

    New, add method to perform user defined actions
    Add perform_action method that allows to perform predefined
    actions with some aditional checks and delays for page
    or its elements to safely load
    
    Parcialy improved work with the templates of pages.

--- webtest/webagent.py
+++ webtest/webagent.py
@@ -12,21 +12,22 @@ import logging  # python logging module
 import itertools  # iteration tool of python
 import re  # regex
 import hashlib  # hashing algorithms for dynamic class path
-import time
 
 import pudb  # debuger
 
-import time
+from time import sleep
 # from unittest.mock import Mock
-
-from webtest.browsers.browser import Browser
+# from webtest.actions.user_action import UserAction
+# from webtest.browsers.browser import Browser
 from webtest.browsers.chrome import Chrome
 from webtest.common.logger import get_logger
 from webtest.common.http import Constants as HTTP_CONST
 from webtest.components.models.google.google import GoogleMainPage
+from webtest.components.models.hawtio.LoginPage import HawtioLoginPageNS
 from webtest.components.pagemodel import model
 from webtest.components.pagemodel.model import PageModel
 from webtest.components.pagemodel.page import Page
+from webtest.components.pagemodel.component import Component
 
 # import pytest
 # from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, WebDriverException
@@ -51,18 +52,33 @@ class WebAgent(object):
     BROWSER_PATH = None
     DRIVER_PATH = None
 
-    def __init__(self):
+    def __init__(self, template_list: list=list()):
         class_name = str(self.__class__.__name__)
         self._logger = get_logger(class_name)
 
-        self._host = None
-        self._port = None
-        self._url = None
-        self._protocol = None
-        self._browser = None
+        self._browser = None # type: Browser
         self._page = None
+        self._templates_by_url = dict()
+        self._templates_by_name = dict()
 
-    def start_up_browser(self, browser: str =BROWSER_CHROME):
+        for template in template_list: # type: PageModel
+            self._templates_by_url[template.url] = template
+            self._templates_by_name[template.name] = template
+
+    def start_up_browser(self, browser: str =BROWSER_CHROME) -> None:
+        if self._browser is not None and self._browser.name.lower() == browser:
+            if self._browser.is_running:
+                self._logger.debug("Selected browser is already running.")
+                return
+            else:
+                self._logger.debug("Starting up already existing instance of browser.")
+                self._browser.start_webdriver()
+                return
+
+        elif self._browser is not None and self._browser.is_running:
+            self._logger.debug("Exchanging browser instances.")
+            self._browser.quit()
+
         if browser == WebAgent.BROWSER_CHROME:
             self._browser = Chrome()
         
@@ -85,52 +101,79 @@ class WebAgent(object):
 
         self._logger.debug("Launching webdriver for '{}' browser.".format(browser))
         self._browser.start_webdriver()
+        return
+
+    def close_browser(self) -> None:
+        self._logger.debug("Closing webbrowser.")
+        self._browser.quit()
+        return
+
+    def is_browser_running(self):
+        if self._browser is not None:
+            return self._browser.is_running
+
+        return False
 
     def go_to_page(self,
             host: str,
             port: int = 0, # 8161,
             url: str = "/",
-            protocol: str =HTTP_CONST.PROTOCOL_HTTP):
+            protocol: str =HTTP_CONST.PROTOCOL_HTTP) -> Page or None:
 
         self._logger.debug("Creating new page model without any components.")
         model = PageModel(protocol=protocol, host=host, port=port, url=url)
 
         self._logger.debug("Connecting to '{}' via this address.".format(model.url))
-        return self.get_page(model=model)
+        return self.get_page(page_model=model)
 
-    def get_page(self, model: PageModel=None, full_url: str=None):
-        if model is not None:
+    def get_page(self, page_model: PageModel=None, full_url: str=None, name=None) -> Page or None:
+        if page_model is not None:
             debug_message = "Getting page '{}' based on predefined model/template."
-            self._logger.debug(debug_message.format(model.url))
-            return self._browser.get_page(model=model)
+            self._logger.debug(debug_message.format(page_model.url))
+
         elif full_url is not None and len(full_url) > 0:
-            debug_message = "Getting page '{}' based on url only."
-            self._logger.debug(debug_message.format(model.url))
-            return self._browser.get_page(model=model)
+            debug_message = "Getting page '{}' based on url."
+            self._logger.debug(debug_message.format(full_url))
+
+            if full_url not in self._templates_by_url.keys():
+                page_model = Page(self._browser)
+                self._logger.warning("No page template been supplied for given url.")
+            else:
+                self._logger.info("Page model found for given url.")
+                page_model = self._templates_by_url[full_url]
+
+        elif name is not None and name in self._templates_by_name.keys():
+            self._logger.info("Page model found for given name.")
+            page_model = self._templates_by_name[name]
         else:
-            self._logger.warning("No page template or url has been supplied to get_page() method.")
-            return None
+            self._logger.error("No page template or url has been supplied to get_page() method.")
+            self._page = None
 
+        if page_model is not None:
+            self._page = self._browser.get_page(model=page_model)
 
-    def close_browser(self):
-        self._logger.debug("Closing webbrowser.")
-        self._browser.quit()
+        return self._page
 
+    def get_current_page(self) -> Page or None:
+        return self._page
+
+    def perform_action(self, action: 'UserAction') -> bool:
+        success = action.perform_self(self)
+        if action.expected_redirection():
+            action.wait_for_frameworks(self)
+
+        if action.expected_content_change():
+            action.wait_for_frameworks(self)
+
+        return success
+
+    def perform_action_get(self, action: 'UserAction') -> object or None:
+        return action.perform_self(self)
 
 
 # main ________________________________________________________________________
 def main():
-    agent = WebAgent()
-
-    agent.start_up_browser()
-    agent.get_page(GoogleMainPage())
-    time.sleep(5)
-    agent.close_browser()
-
-    agent.start_up_browser()
-    agent.go_to_page("www.google.cz")
-    time.sleep(5)
-    agent.close_browser()
+    pass
 
 
 if __name__ == '__main__':
