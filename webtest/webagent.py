d8f6c363296f77a9cc969f5ed79f9a067a73b965
Roman Vais <rvais@redhat.com>
2017-09-14 11:03:58 +0200

    Update WebAgent perform action method
    Update webagent in a manner so it can deal with multiple templates
    and by extension with multiple pages that test can visit by
    e.g. clicking on a link.

--- webtest/webagent.py
+++ webtest/webagent.py
@@ -21,7 +21,7 @@ from time import sleep
 # from webtest.browsers.browser import Browser
 from webtest.browsers.chrome import Chrome
 from webtest.common.logger import get_logger
-from webtest.common.http import Constants as HTTP_CONST
+from webtest.common.http import Constants as HTTP_CONST, relax_url, cut_host_from_url
 from webtest.components.models.google.google import GoogleMainPage
 from webtest.components.models.hawtio.LoginPage import HawtioLoginPageNS
 from webtest.components.pagemodel import model
@@ -147,23 +147,51 @@ class WebAgent(object):
             page_model = self._templates_by_name[name]
         else:
             self._logger.error("No page template or url has been supplied to get_page() method.")
-            self._page = None
+            page = None
 
         if page_model is not None:
-            self._page = self._browser.get_page(model=page_model)
+            page = self._browser.get_page(model=page_model)
 
-        return self._page
+        return page
 
     def get_current_page(self) -> Page or None:
-        return self._page
+        return self._browser.get_current_page
 
     def perform_action(self, action: 'UserAction') -> bool:
         success = action.perform_self(self)
+        waited = False
         if action.expected_redirection():
+            waited = True
             action.wait_for_frameworks(self)
 
-        if action.expected_content_change():
+            url = relax_url(self._browser.current_url)
+
+            if url in self._templates_by_url.keys():
+                model = self._templates_by_url[url]
+            else:
+                self._logger.debug(str(self._templates_by_url.keys()))
+                self._logger.debug(url)
+
+                h = hashlib.sha256()
+                current_model = self._browser.get_current_page.model
+
+                h.update(current_model.name.encode('utf-8'))
+                h.update(current_model.url.encode('utf-8'))
+                h.update(url.encode('utf-8'))
+
+                name = "{}-{}".format(current_model.name, h.hexdigest())
+
+                model = current_model.derive_template(name, url=cut_host_from_url(url))
+                self._templates_by_url[url] = model
+                self._templates_by_name[name] = model
+
+            self._browser.get_page(model=model, update=True)
+
+        if action.expected_content_change() and not waited:
             action.wait_for_frameworks(self)
+
+        if action.delay_for_user_to_see():
+            sleep(5)
 
         return success
 
