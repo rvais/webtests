36fa08d49f06934d44e5d15e024f6ce5b9bfd361
Roman Vais <rvais@redhat.com>
2017-09-14 11:08:10 +0200

    Update current test case in test runner

--- webtest/runner.py
+++ webtest/runner.py
@@ -8,13 +8,23 @@ from webtest.webagent import WebAgent
 from webtest.actions.common.open_browser import OpenBrowser
 from webtest.actions.common.visit_page import VisitPageByName
 from webtest.actions.common.close_browser import CloseBrowser
-from webtest.actions.common.click import ClickOnComponent
+from webtest.actions.common.click import ClickOnComponent, ClickOnLink
 from webtest.actions.common.fill_form import FillForm
 from webtest.components.models.hawtio.LoginPage import HawtioLoginPageNS
+from webtest.components.models.hawtio.attributes_page import HawtioArtemisPage
 
 # main ________________________________________________________________________
 def main():
-    models = [HawtioLoginPageNS(), ]
+
+    login_page = HawtioLoginPageNS()
+    artemis_page = HawtioArtemisPage()
+    models = [
+        login_page,
+        login_page.derive_template(name="HawtioAlternativeLogin", url="console/login"),
+        artemis_page,
+        artemis_page.derive_template(name="HawtioArtemisJMXattributes", url="console/jmx/attributes"),
+    ]
+
     agent = WebAgent(models)
 
     login = {
@@ -27,7 +37,14 @@ def main():
     success = success and agent.perform_action(OpenBrowser())
     success = success and agent.perform_action(VisitPageByName("HawtioLoginPageNS"))
     success = success and agent.perform_action(FillForm(login, 'main', 'login-form'))
-    success = success and agent.perform_action(ClickOnComponent('main', 'login-form', 'send-button'))
+    click = ClickOnComponent('main', 'login-form', 'send-button')
+    click.set_expected_redirection(True)
+    success = success and agent.perform_action(click)
+    click = ClickOnComponent('main', 'navigation', 'preferences')
+    success = success and agent.perform_action(click)
+    click = ClickOnLink("Log out", 'main', 'navigation', 'preferences')
+
+    success = success and agent.perform_action(click)
     success = success and agent.perform_action(CloseBrowser())
 
     assert success
