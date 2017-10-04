9d7c4bb97e732ea65ace21761782b8ed8444919d
Roman Vais <rvais@redhat.com>
2017-10-04 18:36:44 +0200

    New method FillSpecificElement
    In some cases there is need to fill one single input field or textarea.
    Using method FillForm is not comfortable to use in such cases.

--- webtest/actions/common/fill_form.py
+++ webtest/actions/common/fill_form.py
@@ -57,3 +57,52 @@ class FillSpecificInputField(UserAction):
             except Exception as ex:
                 self.action_failure(ex)
                 return False
+
+class FillSpecificElement(UserAction):
+    def __init__(self, tagname: str, attributes: dict=None, value: str or list or bool='',  component_name: str = Page.ROOT_COMPONENT_NAME, *args, **kwargs):
+        super(FillSpecificElement, self).__init__(**kwargs)
+
+        self._tag = tagname
+        self._attributes = attributes if attributes is not None and isinstance(attributes, dict) else dict()
+        self._value = value
+        self._component = component_name
+        self._subcomponents = list()
+        if len(args) > 0:
+            self._subcomponents.extend(list(args))
+
+    def perform_self(self, agent: 'WebAgent') -> bool:
+            page = agent.get_current_page()  # type: Page
+            try:
+                self._logger.info("Filling specified element <{}> with value '{}' .".format(self._tag, self._value))
+                component = UserAction._get_component(page, self._component, self._subcomponents)
+                node = component.get_element_node() # type: Element
+                if node.tag_name == self._tag:
+                    success = True
+                    for attr in  self._attributes.keys():
+                        v = node.get_attribute(node, attr)
+                        if self._attributes[attr] != v:
+                            self._logger.debug('<{tag} {attr}="{value}"> found vs. '
+                                               '<{tag} {attr}="{rq}"> required'.format(tag=self._tag, attr=attr, value=v, rq=self._attributes[attr]))
+                            success = False
+
+                    if not success:
+                        self.action_failure(msg="Element found does not have all required attributes or some of the values are different.")
+                        return success
+
+                    return node.fill(self._value)
+                else:
+                    attr_list = list()
+                    for attr in self._attributes.keys():
+                        attr_list.append('@{attr}="{value}"'.format(attr=attr, value=self._attributes[attr]))
+
+                    if len(attr_list) > 0:
+                        selector = '[{}]'.format(' and '.join(attr_list))
+                    else:
+                        selector = ''
+
+                    node = node.get_element_by_xpath("//*/{}{}".format(self._tag, selector))
+                    return node.fill(self._value)
+
+            except Exception as ex:
+                self.action_failure(ex)
+                return False
