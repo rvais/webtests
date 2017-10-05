12c83fde187ff07351e0582e1b8d2cf455a69669
Roman Vais <rvais@redhat.com>
2017-10-05 20:26:19 +0200

    Add documentation comments to PageModel

--- webtest/components/pagemodel/model.py
+++ webtest/components/pagemodel/model.py
@@ -10,8 +10,26 @@ from webtest.common.http import URL
 from webtest.components.pagemodel.component import Component
 from webtest.common.logger import get_logger
 
+#
+# PageModel class represents knowledge about the web page that user has
+# available. Namely it is a templates of the website and URL address by
+# which to access it. Class contains implementation of methods that are
+# used for interaction with the template, but actual template is empty.
+# To create new template this page should be subclassed and method
+# _create_template() overridden.
+#
 class PageModel(dict):
-
+    #
+    # Constructor. Method requires URL information so page can be paired with template.
+    # Multiple pages with different URLs to access can share the same template but not
+    # the same instance of template. (Can be improved upon in the future)
+    #
+    # @param host: hostname that is part of URL (e.g. www.example.com or IP address)
+    # @param port: port number in case it is not standard port 80
+    #              value 0 means not uo use port number in URL
+    # @param uri: string that is usually part of HTTP GET request
+    # @param protocol: in some cases called schema - "http" or "https"
+    #
     def __init__(self, protocol: str=HTTP_CONST.PROTOCOL_HTTP, host: str="localhost", port: int=0, uri: str="/", *arg, **kwargs):
         super(PageModel, self).__init__(*arg, **kwargs)
 
@@ -66,28 +84,64 @@ class PageModel(dict):
     def __set_name(self, name: str):
         self._name = name
 
+    #
+    # Method is supposed to return list of tuples, that are representing page components.
+    # @param None
+    # @return list of tuples representing template of page
+    #
     def _create_template(self):
         return list()
 
+    #
+    # Template's name. Class name by default.
+    #
     @property
     def name(self):
         return self._name
 
+    #
+    # String representing URL that you can see in address bar in the browser.
+    #
     @property
     def url_complete(self) -> str:
         return self._url.format()
 
+    #
+    # String representing URL that you can see in address bar in the browser
+    # without query part. (what follows after '?' or '#' characters)
+    #
     @property
     def url_short(self) -> str:
         return self._url.format()
 
+    #
+    # Object representing URL, instance of URL class.
+    #
     @property
     def url(self):
         return self._url
 
+
+    #
+    # Method returns list of individual parts of web page as they are defined
+    # in template - their Component class instance representation.
+    # @param None
+    # @return list of Component instances or empty list
+    #
     def get_component_list(self):
         return copy(self._component_list)
 
+    #
+    # Method creates duplicate of this Page model with different name and possibly
+    # with different URL location.
+    #
+    # @param host: hostname that is part of URL (e.g. www.example.com or IP address)
+    # @param port: port number in case it is not standard port 80
+    #              value 0 means not uo use port number in URL
+    # @param uri: string that is usually part of HTTP GET request
+    # @param protocol: in some cases called schema - "http" or "https"
+    # @return instance of PageModel, with duplicate of this one's template
+    #
     def derive_template(
             self,
             name: str,
