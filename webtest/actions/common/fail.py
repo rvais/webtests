ef3da2a1871d0344bc8d8d52e1f054b56db98f7d
Roman Vais <rvais@redhat.com>
2017-10-05 18:03:12 +0200

    New user action FailAction
    Add always failing action for testing purposes or for some test's that
    you need for some reason to fail,

---
+++ webtest/actions/common/fail.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# Framework for testing web applications - proof of concept
+# Authors:  Roman Vais <rvais@redhat.com>
+#
+
+from webtest.actions.user_action import UserAction
+from webtest.webagent import WebAgent
+
+class FailAction(UserAction):
+    def __init__(self, *args, **kwargs):
+        super(FailAction, self).__init__(*args, **kwargs)
+        self._redirection = False
+        self._change = False
+        self._delay_for_user = 0
+
+    def perform_self(self, agent: 'WebAgent') -> bool:
+        self._logger.info("Trying to fail and trying hard!")
+        self.action_failure(msg="Successfully failed!")
+        return False
+
