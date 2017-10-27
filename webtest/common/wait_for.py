3dc8761038f616a2ca315e237e231274b1de0be7
Roman Vais <rvais@redhat.com>
2017-10-27 15:37:06 +0200

    Change Wait decorator configuration internals
    Because decorator instances are created when the code of decorated
    class/method is read, code taking care of configuration doesn't
    have a chance to run before that. Due to that fact, configuration
    responsibility has been of loaded to private class and Wait decorator
    now behavies as semi-singleton. Configuration and logger is contained
    in private class "static instance" and decorated method is sontained
    in decorator instance it self.
    
    It allows to configure decorator globally even thou we don't have
    references for individual decorator instances.

--- webtest/common/wait_for.py
+++ webtest/common/wait_for.py
@@ -4,7 +4,7 @@
 # Authors:  Roman Vais <rvais@redhat.com>
 #
 
-
+from copy import copy
 from time import time, sleep
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from webtest.common.logger import get_logger
@@ -12,7 +12,7 @@ from webtest.config.configurator import Configurator
 
 class Wait(object) :
 
-    # global profile of waiting for conditions
+    # global profile of waiting for conditions, best known settings
     __profile__ = {
         'exception-list' : [NoSuchElementException, StaleElementReferenceException],
         'polling-period' : 2,
@@ -20,42 +20,25 @@ class Wait(object) :
         'max_attempts' : 3,
     }
 
-    def __init__(self, condition: callable): #, *args, **kwargs):
-        # setup logger
-        class_name = str(self.__class__.__name__)
-        self._logger = get_logger(class_name)
+    # config has to be implemented as if decorator was singleton
+    # it allows to configure decorator globally even thou we don't have
+    # references for individual decorator instances
+    _config_holder = None
 
+    def __init__(self, condition: callable): #, *args, **kwargs):
         # function to wrap
         self._condition = condition
 
-        # accepting global profile
-        self._exception_list = Wait.__profile__['exception-list']
-        self._polling_period = Wait.__profile__['polling-period']
-        self._timeout = Wait.__profile__['timeout']
-        self._max = Wait.__profile__['max_attempts']
-        self._limited = self._max >= 0 or self._timeout > 0
+        # configuration holder
+        if Wait._config_holder is None:
+            Wait._config_holder = Wait.__Wait()
 
-        # log current settings
-        self._logger.trace("Initialized wait conditions to following values:")
-        self._logger.trace("'{}':  {}". format('exception-list', self._exception_list))
-        self._logger.trace("'{}':  {}". format('polling-period', self._polling_period))
-        self._logger.trace("'{}':  {}". format('timeout', self._timeout))
-        self._logger.trace("'{}':  {}". format('max_attempts', self._max))
-
-        # crete tuple of exceptions that can be used in try-except statement
-        self._exceptions = tuple(self._exception_list)
-
+        self._config = Wait._config_holder
 
 
     def __call__(self, *args, **kwargs):
-        # customize setting according to configuration
-        cfg = Configurator()
-        self._polling_period = cfg.get_option('attempt_poling_period')
-        self._timeout = cfg.get_option('attempt_timeout')
-        self._max = cfg.get_option('attempt_max_count')
-        self._limited = self._max >= 0 or self._timeout > 0
-
-        self._logger.trace("Waiting on condition ...")
+        logger = self._config.logger
+        logger.trace("Waiting on condition ...")
         debug_print = "Wait call\n{}"
         for x in args:
             debug_print += "\n{}"
@@ -65,11 +48,11 @@ class Wait(object) :
                 if x is not None:
                     debug_print += "\n{" + x + "}"
 
-            self._logger.trace(debug_print.format(self, *args, **kwargs))
+            logger.trace(debug_print.format(self, *args, **kwargs))
         else:
-            self._logger.trace(debug_print.format(self, *args))
+            logger.trace(debug_print.format(self, *args))
 
-        timer = time() + self._timeout
+        timer = time() + self._config.timeout
         counter = 0
         while True:
             counter += 1
@@ -80,15 +63,100 @@ class Wait(object) :
                 return result
                 # else:
                 #    self._logger.warning("Condition on result failed. Waiting another period ...")
-            except self._exceptions as ex:
-                if self._limited and (timer <= time() or counter >= self._max):
-                    self._logger.warning("All possible attempts or timer on condition "
+            except self._config.exception_list as ex:
+                if self._config.limited and (timer <= time() or counter >= self._config.max_attempts):
+                    logger.warning("All possible attempts or timer on condition "
                                          "were exhausted. Re-rising exception.")
                     raise ex
 
-                self._logger.debug("Unsuccessful attempt {} to gain result. Waiting another period ...".format(counter))
-                sleep(self._polling_period)
+                logger.debug("Unsuccessful attempt {} to gain result. Waiting another period ...".format(counter))
+                sleep(self._config.polling_period)
 
             except Exception as ex:
                 raise ex
 
+    @staticmethod
+    def set_options(cfg: dict or Configurator):
+        # configuration holder - there is no guarantee that it already exists
+        if Wait._config_holder is None:
+            Wait._config_holder = Wait.__Wait()
+
+        # gain reference for config holder instance
+        holder = Wait._config_holder
+
+        # customize setting according to given configuration
+        if isinstance(cfg, Configurator):
+            holder.polling_period = cfg.get_option('attempt_polling_period')
+            holder.timeout = cfg.get_option('attempt_timeout')
+            holder.max_attempts = cfg.get_option('attempt_max_count')
+
+        elif isinstance(cfg, dict):
+            holder.polling_period = cfg['polling_period']
+            holder.timeout = cfg['timeout']
+            holder.max_attempts = cfg['attempt_max_count']
+
+
+    # Private class serving as place holder for configuration
+    class __Wait(object) :
+
+        def __init__(self): #, *args, **kwargs):
+            # setup logger
+            self._class_name = str(self.__class__.__name__).strip("_")
+            self._logger = None
+
+            # accepting global profile settings
+            self._exception_list = Wait.__profile__['exception-list']
+            self._polling_period = Wait.__profile__['polling-period']
+            self._timeout = Wait.__profile__['timeout']
+            self._max = Wait.__profile__['max_attempts']
+
+            # crete tuple of exceptions that can be used in try-except statement
+            self._exceptions = tuple(self._exception_list)
+
+
+        @property
+        def exception_list(self) -> list:
+            return copy(self._exception_list)
+
+
+        @property
+        def polling_period(self) -> int or float:
+            return self._polling_period
+
+
+        @property
+        def timeout(self) -> int or float:
+            return self._timeout
+
+
+        @property
+        def max_attempts(self) -> int:
+            return self._max
+
+
+        @property
+        def is_limited(self) -> bool:
+            return self._max >= 0 or self._timeout > 0
+
+
+        @property
+        def logger(self):
+            if self._logger is None:
+                self._logger = get_logger(self._class_name)
+
+            return self._logger
+
+
+        @polling_period.setter
+        def polling_period(self, val:int or float):
+            self._polling_period = val
+
+
+        @timeout.setter
+        def timeout(self, value:int or float):
+            self._timeout = value
+
+
+        @max_attempts.setter
+        def max_attempts(self, value:int):
+            self._max = value
